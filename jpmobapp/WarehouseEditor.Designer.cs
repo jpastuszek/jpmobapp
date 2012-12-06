f37d9ddd94773d8a1d0bfd0fa93bf4dc7b0c067b
Jakub Pastuszek <jpastuszek@gmail.com>
2012-12-06 18:30:30 +0100

    initial schema and app trying to access it but no DB tables

--- jpmobapp/WarehouseEditor.Designer.cs
+++ jpmobapp/WarehouseEditor.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace jpmobapp
 {
-    partial class Form1
+    partial class WarehouseEditor
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(31, 50);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(293, 183);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // WarehouseEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(660, 452);
+            this.Controls.Add(this.listView1);
+            this.Name = "WarehouseEditor";
+            this.Text = "Warehouse Editor";
+            this.Load += new System.EventHandler(this.WarehouseEditor_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
---
+++ jpmobapp/WarehouseEditor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace jpmobapp
+{
+    public partial class WarehouseEditor : Form
+    {
+        public WarehouseEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void WarehouseEditor_Load(object sender, EventArgs e)
+        {
+            using (var context = new WarehouseContext())
+            {
+                Debug.WriteLine("Products listing obtained");
+
+                var products =
+                    from product in context.Products
+                    orderby product.Name
+                    select product;
+                Debug.WriteLine("Products listing obtained");
+                foreach (var product in products)
+                {
+                    Debug.WriteLine(product.Name);
+                }
+
+                //Debug.WriteLine("Found products: " + products.Count().ToString());
+                Debug.WriteLine("Products listing updated");
+
+            }
+        }
+    }
+}
